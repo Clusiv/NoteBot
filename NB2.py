6ed693b02a12096e4d32be6a03d4884dce8a5aaa
Imran <sys_adm95@inbox.ru>
2021-04-23 04:35:43 +0300

    something work

--- NB2.py
+++ NB2.py
@@ -1,5 +1,9 @@
-class NoteBook:
-    def __init__(self, name):
+import tkinter as tk
+from tkinter import ttk
+
+class NoteBook(ttk.Button):
+    def __init__(self, label, name, master):
+        tk.Button.__init__(self, text=label, master = master, bg='black')
         self.name = name
         self.book = {}
         self.count = 0
@@ -7,9 +11,9 @@ class NoteBook:
     def __repr__(self):
         return self.name
 
-    def create_note(self, name, txt):
+    def create_note(self, name, txt, master):
         self.count += 1
-        self.book[self.count] = Note(self.count, name, txt)
+        self.book[self.count] = Note(self.count, name, txt, master=master)
         return self.count
     
     def get_note(self, id):
@@ -21,12 +25,18 @@ class NoteBook:
                 return self.book[note_id]
         return "Nothing found"
 
-        searched_notes = set(searched_notes)
-        return ''.join(searched_notes) + '\n'
+        # searched_notes = set(searched_notes)
+        # return ''.join(searched_notes) + '\n'
 
-class Note:
-    def __init__(self, id, name, txt):
+    def get_all(self):
+        return self.book
+
+class Note(tk.Button):
+    def __init__(self, id, name, txt, master):
+        tk.Button.__init__(self, text = name, master = master, bg='#ffb3fe')
         self.id = id
         self.name = name
         self.txt = txt
-    
+
+if __name__ == '__main__':
+    pass
--- tk-app.py
+++ tk-app.py
@@ -1,17 +1,10 @@
 import tkinter as tk
+from tkinter import END
+
+from tkinter import ttk
+
 import time
 from NB2 import NoteBook
-
-
-notebook1 = NoteBook('Code Snippets')
-notebook1.create_note('C# Snippets', 'Snippets Python')
-notebook1.create_note('Python Snippets', 'Snippets C#')
-
-notebook2 = NoteBook('Life Hacks')
-notebook2.create_note('Morning routine', 'Tea, gym, ...')
-notebook2.create_note('Health', 'Drink water')
-
-notebooks = {1:notebook1, 2:notebook2}
 
 window = tk.Tk()
 
@@ -20,30 +13,49 @@ window.title("NoteBook")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(2, minsize=800, weight=1)
 
+window.configure(bg='#494949')
+
 notebooks_frame = tk.Frame(window)
+notebooks_frame.config(background = '#494949', )
+
 notes_frame = tk.Frame(window)
+notes_frame.config(background = '#494949')
+
 txt_edit_frame = tk.Frame(window)
+txt_edit_frame.config(background = '#494949')
 
-notebooks_frame.grid(row=0, column=0,  padx=5)
-notes_frame.grid(row=0, column=1,  padx=5)
-txt_edit_frame.grid(row=0, column=2)
-
-def handle_click(event,notebook):
-    for note in notebook:
-        note_widget = tk.Label(master=notes_frame, text=note.id)
-        note_widget.pack()
-
-for notebook in notebooks:
-    notebook_widget = tk.Label(master=notebooks_frame, text=notebooks[notebook])
-    notebook_widget.pack()
-    notebook_widget.bind("<Button-1>", lambda event, obj=notebook: handle_click(event, notebook))
-
-
-notes = tk.Label(master=notes_frame, text="I'm in Frame b")
 txt_edit = tk.Text(master=txt_edit_frame)
 
+notebook1 = NoteBook('Code Snippets','Code Snippets', master=notebooks_frame)
+notebook1.create_note('C# Snippets', 'Snippets C#', master=notes_frame)
+notebook1.create_note('Python Snippets', 'Snippets Python', master=notes_frame)
 
-notes.pack()
+notebook2 = NoteBook('Life Hacks', 'Life Hacks', master=notebooks_frame)
+notebook2.create_note('Morning routine', 'Tea, gym, ...', master=notes_frame)
+notebook2.create_note('Health', 'Drink water', master=notes_frame)
+
+notebooks = [notebook1, notebook2]
+
+
+notebooks_frame.grid(row=0, column=0,  padx=5, sticky=tk.NSEW)
+notes_frame.grid(row=0, column=1,  padx=5, sticky=tk.NSEW)
+txt_edit_frame.grid(row=0, column=2, sticky=tk.NSEW)
+
+def show_notes(event, notebook):
+    for index, note in notebook.book.items():
+        note.bind("<Button-1>", lambda event, widget=note: show_note(event, widget))
+        note.pack()
+
+def show_note(event, note):
+    txt_edit.delete(1.0, END)
+    txt_edit.insert(1.0, note.txt)
+
+for notebook in notebooks:
+    notebook.bind("<Button-1>", lambda event, widget=notebook: show_notes(event, widget))
+    notebook.pack()
+
+
+
 txt_edit.pack()
 
 window.mainloop()
